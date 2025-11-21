ad1e2e5058ff225bca66d9f7d3f13da788f8a1c5
Moises-salas-R <MOISESSAL4S727@GMAIL.COM>
2025-11-20 21:22:18 -0500

    completed

--- src/Calculadora.py
+++ src/Calculadora.py
@@ -1,1 +1,46 @@
+def Sumar(num1, num2):
+    return num1 + num2
+
+def Restar(num1, num2):
+    return num1 - num2
+
+def Multiplicar(num1, num2):
+    return num1 * num2
+
+def Dividir(num1, num2):
+    if num2 == 0:
+        raise ValueError("error: el número es cero")
+    return num1 / num2
+
+if __name__ == "__main__": # pragma: no cover
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    print("operaciones")
+    print("1:suma")
+    print("2:resta")
+    print("3:multiplicación")
+    print("4:división")
+
+    operación = input("elige una operación: ")
+
+    if (operación == "1"):
+        resultado = num1 + num2
+        print(resultado)
+    elif (operación == "2"):
+        resultado = num1 - num2
+        print(resultado)
+    elif (operación == "3"):
+        resultado = num1 * num2
+        print(resultado)
+    elif (operación == "4"):
+        resultado = num1 / num2
+        if (num2 != 0):
+            print(resultado)
+        else:
+            print("error: el número es cero")
+    else:
+        print("números inválidos")
+
+
 
--- tests/test_Calculadora.py
+++ tests/test_Calculadora.py
@@ -1,1 +1,34 @@
+import pytest
 
+from src.Calculadora import Dividir, Multiplicar, Restar, Sumar
+
+
+def test_Sumar():
+    assert Sumar(3, 5) == 8
+    assert Sumar(-2, 2) == 0
+    assert Sumar(0, 0) == 0
+    assert Sumar(7, -2) == 5
+    assert Sumar(-1, -10) == -11
+
+def test_Restar():
+    assert Restar(10, 5) == 5
+    assert Restar(0, 0) == 0
+    assert Restar(-3, -2) == -1
+    assert Restar(-15, 2) == -17
+    assert Restar(-20, -20) == 0
+
+def test_Multiplicar():
+    assert Multiplicar(4, 5) == 20
+    assert Multiplicar(-2, 3) == -6
+    assert Multiplicar(0, 10) == 0
+    assert Multiplicar(0, 0) == 0
+    assert Multiplicar(-2, -2) == 4
+
+def test_Dividir():
+    assert Dividir(10, 2) == 5
+    assert Dividir(-6, -2) == 3
+    assert Dividir(-8, -4) == 2
+
+def test_Dividir_por_cero():
+    with pytest.raises(ValueError):
+        Dividir(5, 0)
